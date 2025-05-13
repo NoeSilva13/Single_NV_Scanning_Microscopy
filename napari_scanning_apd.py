37165f2e8b35e81567bd6dfd44c1d5f32c406840
burkelabuci <burkelabuci@gmail.com>
2025-05-13 12:17:19 -0700

    APD experiments configurations

--- napari_scanning_apd.py
+++ napari_scanning_apd.py
@@ -53,9 +53,9 @@ def scan_pattern(x_points, y_points):
         for y_idx, y in enumerate(y_points):
             for x_idx, x in enumerate(x_points):
                 ao_task.write([x, y])
-                time.sleep(0.01)
+                time.sleep(0.001)
                 voltage = ai_task.read()
-                #print(voltage)
+                print(voltage)
                 image[y_idx, x_idx] = voltage
                 layer.data = image
     layer.contrast_limits = (np.min(image), np.max(image))
